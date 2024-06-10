4e8234fc550e8298c5c9916346149fc5c33c66be
Veronica <sandrinveronica@gmail.com>
2024-06-10 15:53:21 +0200

    Condizioni if/else

---
+++ main.js
@@ -0,0 +1,15 @@
+let v = 15;
+
+if (v < 18){
+    console.log('Insufficente');
+}else if(v >= 18 && v < 21){
+    console.log('sufficiente');
+}else if(v >= 21 && v < 24){
+    console.log('buono');
+}else if (v >= 24 && v < 27){
+    console.log('distinto');
+}else if(v >= 27 && v <= 29){
+    console.log('Ottimo');
+}else if (v = 30){
+    console.log('Eccellente');
+}
