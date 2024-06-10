c2831b5102040d9b497c8728b391d0856c82828a
Veronica <sandrinveronica@gmail.com>
2024-06-10 16:28:46 +0200

    Condizione2 if/else

---
+++ main01.js
@@ -0,0 +1,13 @@
+let degree = 20;
+
+if(degree < 20){
+    console.log('non ci sono piu’ le mezze stagioni');
+}else if(degree >= 30){
+    console.log('lu mare, lu sole, lu ientu');
+}else if (degree < 30){
+    console.log('mi dia una peroni sudata');
+}else if(degree < 0){
+    console.log('non e’ tanto il freddo quanto l’umidità');
+}else if (degree < -10){
+    console.log('copriti…ancora ti raffreddi');
+}
